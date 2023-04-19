6c285e3800c1dd1a1a85485c91171c99cbc6bf04
glassbarricades01~ <glassbarricades1@list.ru>
2023-04-19 17:59:37 +0300

    +

--- src/components/AdminPanel/AdminCategory.jsx
+++ src/components/AdminPanel/AdminCategory.jsx
@@ -1,15 +1,35 @@
-import { Button, Group, Title } from "@mantine/core";
-import { useDisclosure } from '@mantine/hooks';
+import { Button, Group, Title, Modal, TextInput } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
 
 const AdminCategory = () => {
+  const [opened, { open, close }] = useDisclosure(false);
+  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+
   return (
     <>
       <Modal opened={opened} onClose={close} title="Добавление категории">
-        
+        <form action="">
+          <TextInput
+            placeholder="Название катерогии"
+            label="Название категории"
+            withAsterisk
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextInput
+            placeholder="Ссылка для меню"
+            label="Ссылка для меню"
+            withAsterisk
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+          />
+        </form>
       </Modal>
       <Group position="apart">
         <Title>Категории меню</Title>
-        <Button>Добавить категорию</Button>
+        <Button onClick={open}>Добавить категорию</Button>
       </Group>
     </>
   );
