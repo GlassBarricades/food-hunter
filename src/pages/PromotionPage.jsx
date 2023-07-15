3853cc74380359db4f8520f18295bd3e1ea7434a
glassbarricades01~ <glassbarricades1@list.ru>
2023-07-15 17:48:08 +0300

    add

--- src/pages/PromotionPage.jsx
+++ src/pages/PromotionPage.jsx
@@ -1,10 +1,16 @@
-import { Title } from "@mantine/core"
+import { Title, SimpleGrid } from "@mantine/core";
+import PromotionCard from "../components/PromotionCard";
 
 const PromotionPage = () => {
-    return (
-        <>
-            <Title>Акции</Title>
-        </>
-    )
-}
+  return (
+    <>
+      <Title mb="xl">Акции</Title>
+      <SimpleGrid cols={3}>
+        <PromotionCard image={"https://i.ibb.co/w63F41f/two-kind-pizza-table.jpg"} title={"Каждый вторник вторая пицца бесплатно"}/>
+        <PromotionCard image={"https://i.ibb.co/F3Vvc6Y/friends-eating-pizza-together-home.jpg"} title={"Каждую среду cкидка 30% на вынос"} />
+        <PromotionCard image={"https://i.ibb.co/Pt4FD0g/cute-friends-cafe-eatting-pizza.jpg"} title={"Именинникам 15% скидка за три дня и после "}/>
+      </SimpleGrid>
+    </>
+  );
+};
 export default PromotionPage;
