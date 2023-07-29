e74911800f52f7f1aac49a1b7120fbcb7e8b7e54
glassbarricades <glassbarricades1@list.ru>
2023-07-30 00:47:10 +0300

    fix immutable bug

--- src/store/orderSlice.js
+++ src/store/orderSlice.js
@@ -7,16 +7,11 @@ const orderSlice = createSlice({
     order: [],
   },
   reducers: {
-    addOrder: (state, action) => {
+    addOrder(state, action) {
+      const {quantity, label, item, id, price} = action.payload
       // setProductsArray([...productsArray, id]);
       // setProductsKolArr([...productsKolArr, String(value)]);
-       let obj = action.payload.item;
-       obj.quantity = action.payload.quantity;
-       obj.variantOrder = action.payload.label;
-       obj.orderId = action.payload.id;
-       obj.priceOrder = action.payload.price;
-      obj.orderUuid = uid();
-      state.order.push(obj);
+      state.order.push({...item, quantity: quantity, variantOrder: label, orderId: id, priceOrder: price, orderUuid: uid()});
     },
     removeOrder(state, action) {
       state.order = state.order.filter(order => order.orderUuid !== action.payload.id);
