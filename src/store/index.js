c76249cda7af8c7494d4b398d54e5dbadfae92aa
glassbarricades <glassbarricades1@list.ru>
2023-08-04 14:55:00 +0300

    add persistor

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
-import orderReducer from './orderSlice';
-import productQuantitySlice from './productQuantitySlice';
-import productsArrSlice from './productsArrSlice';
-import quantitySlice from './quantitySlice';
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import orderReducer from './orderSlice'
+import productQuantitySlice from './productQuantitySlice'
+import productsArrSlice from './productsArrSlice'
+import quantitySlice from './quantitySlice'
 
-export default configureStore({
-  reducer: {
-    order: orderReducer,
-    quantity: quantitySlice,
-    productsArr: productsArrSlice,
-    productQuantity: productQuantitySlice
-  },
-});
+const rootReducer = combineReducers({
+	order: orderReducer,
+	quantity: quantitySlice,
+	productsArr: productsArrSlice,
+	productQuantity: productQuantitySlice,
+})
+
+const persistConfig = {
+	key: 'root',
+	storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+})
+export const persistor = persistStore(store)
+export default store
--- src/store/orderSlice.js
+++ src/store/orderSlice.js
@@ -1,24 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 const orderSlice = createSlice({
-  name: "order",
-  initialState: {
-    order: [],
-  },
-  reducers: {
-    addOrder(state, action) {
-      const {quantity, label, item, id, price, handlers, orderUuid} = action.payload
-      state.order.push({...item, quantity: quantity, variantOrder: label, orderId: id, priceOrder: price, orderUuid: orderUuid});
-      if (typeof handlers === 'object') {
-        handlers.set(0)
-      }
-    },
-    removeOrder(state, action) {
-      state.order = state.order.filter(order => order.orderUuid !== action.payload.id);
-    },
-  },
-});
+	name: 'order',
+	initialState: {
+		order: [],
+	},
+	reducers: {
+		addOrder(state, action) {
+			const { quantity, label, item, id, price, handlers, orderUuid } =
+				action.payload
+			state.order.push({
+				...item,
+				quantity: quantity,
+				variantOrder: label,
+				orderId: id,
+				priceOrder: price,
+				orderUuid: orderUuid,
+			})
+			if (typeof handlers === 'object') {
+				handlers.set(0)
+			} else {
+				return undefined
+			}
+		},
+		removeOrder(state, action) {
+			state.order = state.order.filter(
+				order => order.orderUuid !== action.payload.id
+			)
+		},
+	},
+})
 
-export const { addOrder, removeOrder } = orderSlice.actions;
-
-export default orderSlice.reducer;
+export const { addOrder, removeOrder } = orderSlice.actions
+export default orderSlice.reducer
